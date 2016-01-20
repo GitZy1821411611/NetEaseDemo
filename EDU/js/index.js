6f6a887e1d6d08640364b6a2af6fc4d123a6b790
Mrs.Zhang <1821411611@qq.com>
2016-01-20 10:46:14 +0800

    修改轮播相关代码
    添加轮播js代码以及修改相关css

--- EDU/js/index.js
+++ EDU/js/index.js
@@ -1,6 +1,20 @@
+
 $(function(){
-	// $("#close_topbar").click(function(){
-	// 	$(".barwrapper").slideUp();
-	// 	$(".lunbo").animate({top:"90px"});
-	// })
+	//关闭头部提醒区域
+	$("#close_topbar").click(function(){
+		$(".barwrapper").slideUp();
+		$(".lunbo").animate({top:"90px"});
+	})
+	//轮播代码
+	//获取窗口可视化宽度
+	var imgwidth=$(window).width();
+	$(".lunbo img").css({"width":imgwidth});
+	$(".point_wrapper").css({"width":imgwidth});
+	setInterval(function(){
+		if(parseInt($(".lunbo_img_wrapper").css("left"))==-2*imgwidth){
+			$(".lunbo_img_wrapper").animate({left:"0px"});
+		}else{
+			$(".lunbo_img_wrapper").animate({left:"-="+imgwidth});
+		}	
+	},3500);
 })
